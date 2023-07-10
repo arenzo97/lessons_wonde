e822c382d554938abbd59fe00cfdacdeaca782db
Luis Arenas <larenas97@gmail.com>
2023-07-10 16:50:46 +0100

    wording

--- src/resources/views/students.blade.php
+++ src/resources/views/students.blade.php
@@ -4,7 +4,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1">
 
-        <title>My Timetable</title>
+        <title>Students list</title>
         <link rel = "stylesheet" href="{{ asset('css/app.css') }}">
         <script src="https://cdn.tailwindcss.com"></script>
     </head>
